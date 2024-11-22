19cda32bdefcb98c13f40229d00a935fa359f321
Chokokito <miguelferocha@outlook.com>
2024-11-22 20:32:40 -0300

    First Commit

---
+++ chokokito/src/app/page.js
@@ -0,0 +1,16 @@
+import "./globals.css";
+
+export default function Home() {
+  return (
+    <div className=" w-lvw h-lvh flex items-center flex-col justify-evenly">
+      <h1 className="">Welcome to the edge of dreams and reality. 🌙</h1>
+      <div className=" flex flex-col ">
+        <h2>
+          You're a bit early, but I'm glad you're here. <br></br>This space is
+          under construction, just like every great dream.<br></br> Come back
+          soon to see it come alive.
+        </h2>
+      </div>
+    </div>
+  );
+}
