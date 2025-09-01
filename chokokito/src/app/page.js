330ea23a693c275a0b7ddad53349711ab26ccee9
Chokokito <69050917+Chokokito@users.noreply.github.com>
2025-09-01 06:35:27 +0000

    Add OGL library, new DarkVeil component, update main page, and remove unused Babel config

--- chokokito/src/app/page.js
+++ chokokito/src/app/page.js
@@ -1,8 +1,10 @@
+import DarkVeil from "./components/DarkVeil";
 import "./globals.css";
 
 export default function Home() {
   return (
     <div className=" w-lvw h-lvh flex items-center flex-col justify-evenly">
+      <DarkVeil></DarkVeil>
       <h1 className="">Welcome to the edge of dreams and reality. 🌙</h1>
       <div className=" flex flex-col ">
         <h2>
